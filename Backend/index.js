a0922f0ff602f06d54b930fead78dc1dd0c1bfba
Arunima Paunikar <147681796+Aru-14@users.noreply.github.com>
2025-08-26 12:51:33 +0530

    Updated cors()

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,10 @@ const ClauseByClauseExplanation  = require('./Services/ClauseByClauseExplanation
 const{ getDocumentInfo }=require('./Services/getDocumentInfo')
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",      
+  methods: ["GET","POST","PUT","DELETE"]
+}));
 
 connectDB(); // Ensure DB connection is established
 
